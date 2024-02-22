e3a1407bc4ae6ca08d70ea1434379196e893e1da
leyliperea <160614703+leyliperea@users.noreply.github.com>
2024-02-22 07:36:17 -0600

    Create programa01.py

---
+++ programa01.py
@@ -0,0 +1,13 @@
+""" Nombre:Leylani pg
+    matricula:1723110391
+    grupo:TI21
+    Fecha:18/01/2024
+    Descripcion:programa que crrea un objeto
+
+"""
+
+class A:#define una clase de nombre A
+    matricula:None#declara la variable matricula
+    nombre:None#declara la variable nombre
+objeto=A()#crea un objeto de la clase A
+print("objeto.nombre")#imprime el valor de nombre
